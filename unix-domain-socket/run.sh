04e6176ce132c240377d29764ac3b59bb6350112
Eduardo Vieira Falcão <eduardo.vieira.falcao@gmail.com>
2024-03-04 13:57:37 +0100

    Add script to simplify runnin the UDS example

---
+++ unix-domain-socket/run.sh
@@ -0,0 +1,20 @@
+cd ../build
+
+if [ -e in_file ]; then
+    rm in_file
+else
+    touch in_file
+fi
+if [ -e out_file ]; then
+    rm out_file
+else
+    touch out_file
+fi
+
+echo "this content should be in both files" >> in_file
+
+./uds-server > out_file &
+sleep 0.1
+./uds-client < in_file
+
+kill %1
